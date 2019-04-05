7c5f91a8998fbb1044ee86949b9bea78e5e165d9
A170A6B <pablorodriguez12150@gmail.com>
2019-04-05 15:12:39 +0200

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -12,7 +14,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authenticationService: AuthenticationService,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -21,6 +25,14 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.authenticationService.authenticationState.subscribe(state => {
+        if (state) {
+          this.router.navigate(['members', 'dashboard']);
+        } else {
+          this.router.navigate(['login']);
+        }
+      });
     });
   }
 }
