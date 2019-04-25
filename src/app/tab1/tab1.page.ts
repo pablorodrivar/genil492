b39072d96e81de07b58425ff4f69b21dd82abd73
Pablo <pablorodriguez12150@gmail.com>
2019-04-25 16:10:37 +0200

    Refresher implemented.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
-import { ThumbContentPipe } from '../pipes/thumb-content.pipe';
+import { AlertController } from '@ionic/angular';
 import { Router,ActivatedRoute } from '@angular/router';
 import { Storage } from '@ionic/storage';
 
@@ -15,7 +15,8 @@ export class Tab1Page implements OnInit{
   posts: any[] = [];
   user: any;
 
-  constructor(private httpService: HttpService, private router: Router, private storage: Storage) {}
+  constructor(private httpService: HttpService, private router: Router, private storage: Storage,
+    private alertController: AlertController) {}
   
   async ngOnInit() {
     await this.storage.get(USER_KEY).then(val => {
@@ -37,5 +38,24 @@ export class Tab1Page implements OnInit{
 
   addPost() {
     this.router.navigate(["/add-post"]);
+  }
+
+  async showAlert() {
+    const alert = await this.alertController.create({
+      header: 'Colores',
+      message: 'Azul: Colonia\nAmarillo: Lobatos\nVerde: Tropa\nNaranja: Unidad\nRojo: Clan\nCeleste: General',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  doRefresh(event) {
+    setTimeout(() => {
+      this.posts = [];
+      this.ngOnInit();
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);    
   }
 }
